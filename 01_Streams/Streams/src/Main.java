1f4d6b1b2f4e93ec1f1824db65972b11ab5618c5
stanley <panteleevstas@gmail.com>
2023-09-18 23:26:16 +0500

    Stream experiments

---
+++ 01_Streams/Streams/src/Main.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Hello world!");
+        System.in.read();
+        StreamExperiments.CopyPastStreamReader();
+    }
+}
