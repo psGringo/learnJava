5dc82cf4c5ae97b3a3522487ccb4d89f24cfcf1e
panteleevstas <panteleevstas@gmail.com>
2023-09-24 12:19:21 +0500

    Создание проекта со сборщиком gradlew 8.2 и OracleJDK 17
    Создание проекта со сборщиком gradlew 8.2 и OracleJDK 17
    
    Создание проекта со сборщиком gradle 8.2 и open jdk 21

--- 01_Streams/Streams/src/Main.java
+++
@@ -1,9 +0,0 @@
-import java.io.IOException;
-
-public class Main {
-    public static void main(String[] args) throws IOException {
-        System.out.println("Hello world!");
-        System.in.read();
-        StreamExperiments.CopyPastStreamReader();
-    }
-}
