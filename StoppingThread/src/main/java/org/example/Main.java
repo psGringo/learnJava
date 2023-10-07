e93fd7ba5313d4312deacf769f9d8a82b68fd277
stanley <panteleevstas@gmail.com>
2023-10-07 22:30:50 +0500

    Stopping thread examples

---
+++ StoppingThread/src/main/java/org/example/Main.java
@@ -0,0 +1,69 @@
+package org.example;
+
+import java.util.concurrent.ThreadPoolExecutor;
+
+public class Main {
+    public static void main(String[] args) {
+        FirstExample();
+        SecondExample();
+    }
+
+    static void FirstExample() {
+        var task = new Task();
+        new Thread(task).start();
+        try {
+            Thread.sleep(5000);
+            task.cancel();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    static void SecondExample() {
+        var thread = new Thread(new AnotherTask());
+        try {
+            thread.start();
+            Thread.sleep(5000);
+            thread.interrupt();
+        } catch (InterruptedException e) {
+            System.out.println("Thread was interrupted");
+            ;
+        }
+    }
+}
+
+class Task implements Runnable {
+
+    private boolean needCancel = false;
+
+    public void cancel() {
+        needCancel = true;
+    }
+
+    @Override
+    public void run() {
+        while (!needCancel) {
+            try {
+                Thread.sleep(1000);
+                System.out.println("Tik");
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
+
+class AnotherTask implements Runnable {
+
+    @Override
+    public void run() {
+        while (!Thread.currentThread().isInterrupted()) {
+            try {
+                Thread.sleep(1000);
+                System.out.println("Tik2");
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
