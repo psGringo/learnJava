fac6bf7fffc818aedb85c1223fd684f1a83be944
stanley <panteleevstas@gmail.com>
2023-10-14 13:42:26 +0500

    First tries with lanterna

--- LamdaExamples/src/main/java/org/example/Main.java
+++ LamdaExamples/src/main/java/org/example/Main.java
@@ -4,6 +4,9 @@ public class Main {
     public static void main(String[] args) {
 //        AssigningExample.execute();
 //        MethodAsParamExample.execute();
-        ConstructorExample.execute();
+//        ConstructorExample.execute();
+
+//        System.console().writer().
+        System.out.println(Main.class.getClassLoader().getName());
     }
 }
