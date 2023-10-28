00bb5c3795d4edf3d9dfaae36bc1f01d8612ef7b
stanley <panteleevstas@gmail.com>
2023-10-29 00:46:59 +0500

    alienQuest first commit

---
+++ Module3_JavaProfessional/alienQuest/src/main/java/com/example/alien/quest/AlienQuestApplication.java
@@ -0,0 +1,13 @@
+package com.example.alien.quest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlienQuestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlienQuestApplication.class, args);
+	}
+
+}
---
+++ Module3_JavaProfessional/alienQuest/src/test/java/com/example/alien/quest/AlienQuestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.alien.quest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlienQuestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
