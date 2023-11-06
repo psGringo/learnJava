ca1de61ec9b0e3d51a0c14c5e9c70b40e266c8e4
stanley <panteleevstas@gmail.com>
2023-11-06 09:22:26 +0500

    draft connected ui and server through ApiService and nginx

---
+++ draft/ui/src/ApiService/GreetingsApiService.ts
@@ -0,0 +1,20 @@
+import {CancelablePromise, Greeting, GreetingsService} from "../../openapi-generated";
+
+interface IGreetingsApiService {
+    sayHello(): CancelablePromise<any>
+}
+
+export const GreetingsApiService: IGreetingsApiService = class GreetingsApiService {
+
+    public static sayHello(): CancelablePromise<Greeting | Error> {
+        return new CancelablePromise<any>((resolve) => {
+            GreetingsService.sayHello().then((response) => {
+                console.log('here', response);
+                resolve(response);
+            }).catch((e) => {
+                console.log('here error', JSON.stringify(e));
+            })
+        });
+    }
+
+}
--- draft/ui/src/Components/App/App.tsx
+++ draft/ui/src/Components/App/App.tsx
@@ -1,8 +1,26 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import styles from './Styles.less'
+import {GreetingsApiService} from "@/ApiService/GreetingsApiService";
+import {Greeting} from "../../../openapi-generated";
 
 export const App: React.FC = () => {
-    return (<div className={styles.app}>
-        hello world
-    </div>)
+
+    const [greeting, setGreeting] = useState<Greeting> ({message: 'default'});
+
+    useEffect(() => {
+        GreetingsApiService.sayHello().then((result) => {
+            setGreeting(result)
+        })
+    }, [])
+
+    return (
+        <div className={styles.app}>
+            <div>
+                hello world
+            </div>
+
+            <div>
+                {greeting.message}
+            </div>
+        </div>)
 }
