2bce96bf5b6a972232ed31a2ae5860792831475f
stanley <panteleevstas@gmail.com>
2024-06-23 15:04:42 +0500

    04 JdbcExample with autoclosable

--- 04_JDBCExampleMySql/src/main/java/org/example/Main.java
+++ 04_JDBCExampleMySql/src/main/java/org/example/Main.java
@@ -8,15 +8,16 @@ public class Main {
     public static void main(String[] args) throws SQLException {
         System.out.printf("Hello and welcome!");
 
-        Connection connection = DriverManager.getConnection(
+        try (Connection connection = DriverManager.getConnection(
                 "jdbc:mysql://localhost:3307", "root", "root"
-        );
-        Statement statement = connection.createStatement();
-        ResultSet resultSet = statement.executeQuery("select * from library.author");
-        while (resultSet.next()) {
-            int id = resultSet.getInt(1);
-            String name = resultSet.getString(2);
-            System.out.printf("ID: %d, Name: %s\n", id, name);
+        )) {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("select * from library.author");
+            while (resultSet.next()) {
+                int id = resultSet.getInt(1);
+                String name = resultSet.getString(2);
+                System.out.printf("ID: %d, Name: %s\n", id, name);
+            }
         }
     }
 }
